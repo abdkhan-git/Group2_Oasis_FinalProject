0f15c8c6925e02a525d22e6d458cb7396f09fd98
JaN <janbrix.batalla@gmail.com>
2024-11-18 23:30:21 -0500

    SignInScreen Updated

--- app/src/main/java/com/example/group2_oasis_finalproject/SignInScreen.kt
+++ app/src/main/java/com/example/group2_oasis_finalproject/SignInScreen.kt
@@ -1,15 +1,20 @@
 package com.example.group2_oasis_finalproject
 
 import android.widget.Toast
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.LinearProgressIndicator
@@ -24,8 +29,12 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlurEffect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.LinkAnnotation
 import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.text.TextLinkStyles
@@ -35,6 +44,7 @@ import androidx.compose.ui.text.withLink
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
+import com.example.group2_oasis_finalproject.ui.theme.FarmingdaleGreen
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
@@ -42,7 +52,6 @@ import kotlinx.coroutines.delay
 import kotlinx.coroutines.isActive
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
-
 
 @Composable
 fun signinscreen(navController: NavController) {
@@ -54,22 +63,22 @@ fun signinscreen(navController: NavController) {
     var PassedInspection : Boolean = false
     var CoroutineScopeRunning = false
 
+    Column(
+        Modifier.fillMaxWidth(),
+        Arrangement.Bottom,
+        Alignment.CenterHorizontally
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.fsclogorgb),
+            contentDescription = "SignIn Image",
+            modifier = Modifier
+                .padding(start = 10.dp, top = 100.dp, end = 10.dp, bottom = 50.dp)
+        )
 
-    Column(Modifier.fillMaxWidth(), Arrangement.Bottom, Alignment.CenterHorizontally) {
-
-        Spacer(Modifier.padding(20.dp))
-        Text(text = "LOGIN BELOW by entering your User ID (Ram ID or SSN) and your six-digit LOGIN PIN for access to the Farmingdale State College Online Administrative Student Information System (OASIS).")
-        Text(buildAnnotatedString {
-            append("IMPORTANT: Please read and understand the Colleges ")
-            withLink(LinkAnnotation.Url("https://oasis.farmingdale.edu/genhelp/termsofusage.htm", TextLinkStyles(style = SpanStyle(color = Color.Blue)))) { append("Academic Advisement Terms of Usage") }
-            append(" before you login to this system! If you have any questions regarding the advisement process, contact your ")
-            withLink(LinkAnnotation.Url("https://oasis.farmingdale.edu/genhelp/termsofusage.htm", TextLinkStyles(style = SpanStyle(color = Color.Blue)))) { append("Department Contact.") }
-            })
-        Spacer(Modifier.padding(20.dp))
         OutlinedTextField(value = UserRamIDtext, onValueChange = {UserRamIDtext = it}, label = {Text(text = "Ram ID")})
-        Spacer(Modifier.padding(20.dp))
+        Spacer(Modifier.padding(10.dp))
         OutlinedTextField(value = UserPintext, onValueChange = {UserPintext = it}, label = {Text(text = "Six-Digit Login PIN")}, keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.NumberPassword))
-        Spacer(Modifier.padding(20.dp))
+        Spacer(Modifier.padding(10.dp))
         Row{
             OutlinedButton(onClick = {
                 CoroutineScope(Dispatchers.IO).launch {
@@ -81,14 +90,47 @@ fun signinscreen(navController: NavController) {
                         else {  CoroutineScopeRunning = false; Toast.makeText(Context, "Login Failed", Toast.LENGTH_LONG).show() }
                     }
                 }
-            }) { Text(text = "Login")}
-            Spacer(Modifier.padding(20.dp))
-            Button(onClick = { navController.navigate("signUpScreen") }) { Text(text = "Register")}
+            },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White,
+                    contentColor = FarmingdaleGreen
+                )
+            ) { Text(text = "Login")}
+
+            Spacer(Modifier.padding(10.dp))
+
+            Button(
+                onClick = { navController.navigate("signUpScreen") },
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = FarmingdaleGreen,
+                    contentColor = Color.White
+                )
+            ) {
+                Text(text = "Register")
+            }
         }
     }
-    Column(Modifier.fillMaxWidth().fillMaxSize(), Arrangement.Bottom, Alignment.CenterHorizontally) {
-        Text(text = "Please call the Registrar's Office at (934) 420-2776 during business hours (Monday - Friday 9:00am - 4:30pm) if you have difficulty logging in. ")
-        Spacer(Modifier.padding(20.dp))
+
+    Column(
+        Modifier
+            .fillMaxSize()
+            .padding(start = 10.dp, end = 10.dp),
+        Arrangement.Bottom,
+        Alignment.CenterHorizontally
+    ) {
+        Text(text = "LOGIN ABOVE by entering your User ID (Ram ID or SSN) and your six-digit LOGIN PIN for access to the Farmingdale State College Online Administrative Student Information System (OASIS).", modifier = Modifier.padding(5.dp))
+
+        Text(buildAnnotatedString {
+            append("IMPORTANT: Please read and understand the Colleges ")
+            withLink(LinkAnnotation.Url("https://oasis.farmingdale.edu/genhelp/termsofusage.htm",
+                TextLinkStyles(style = SpanStyle(color = Color.Blue)))) { append("Academic Advisement Terms of Usage") }
+            append(" before you login to this system! If you have any questions regarding the advisement process, contact your ")
+            withLink(LinkAnnotation.Url("https://oasis.farmingdale.edu/genhelp/termsofusage.htm",
+                TextLinkStyles(style = SpanStyle(color = Color.Blue)))) { append("Department Contact.") }
+        }, modifier = Modifier.padding(5.dp))
+
+        Text(text = "Please call the Registrar's Office at (934) 420-2776 during business hours (Monday - Friday 9:00am - 4:30pm) if you have difficulty logging in. ", modifier = Modifier.padding(5.dp))
+
         if (CoroutineScopeRunning == true) { LinearProgressIndicator(Modifier.fillMaxWidth())}
     }
 }
